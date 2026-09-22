b6ae724385ba13b63aebd6887e6fe5604a427098
fishCake07 <yookbinfoo0110@gmail.com>
2026-09-22 22:21:41 +0800

    fix:remove malfunction features

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-const CACHE_NAME = "expense-tracker-cache-v82";
+const CACHE_NAME = "expense-tracker-cache-v83";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
-  "./style.css?v=82",
-  "./app.js?v=82",
-  "./auth-sync.js?v=82",
+  "./style.css?v=83",
+  "./app.js?v=83",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
